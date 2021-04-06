132c171c33cdb072a3ff7e9cc012c3350bcd6a16
joon <joon@joon-PC>
2021-04-07 00:24:36 +0900

    00:24 memberDAO

---
+++ src/creation/member/model/dao/MemberDAO.java
@@ -0,0 +1,43 @@
+package creation.member.model.dao;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import creation.common.config.ConfigLocation;
+
+import static creation.common.jdbc.JDBCTemplate.close;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import creation.board.model.dto.HPBoardDTO;
+import creation.common.config.ConfigLocation;
+
+public class MemberDAO {
+	
+	private final Properties prop;
+	
+	public MemberDAO() {
+	
+		prop = new Properties();
+		
+		try {
+			
+			prop.loadFromXML(new FileInputStream(ConfigLocation.MAPPER_LOCATION + "board/faq-mapper.xml"));
+			
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+			
+		}
+		
+	}
+}
